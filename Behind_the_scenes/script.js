c3f2d15869a200f831c1b321be4c1c6e4032ea3c
Szymon Heller <hellerszymon70@gmail.com>
2021-04-12 23:07:42 +0200

    Add hoisting examples.

--- Behind_the_scenes/script.js
+++ Behind_the_scenes/script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 
 function calcAge(birthYear) {
   const age = 2021 - birthYear;
@@ -28,3 +29,52 @@ function calcAge(birthYear) {
 
 const firstName = "Szymon";
 calcAge(1996);
+
+*/
+
+// -- hoisting in practice --
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Szymon";
+let job = "programmer";
+const year = "1996";
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+// Example 2
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
